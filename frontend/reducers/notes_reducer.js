fcbd07f5aec06946c42d85bc1b8ee14807138b2e
brittanyz <brittanyzellman@gmail.com>
2017-08-29 21:08:56 -0400

    pushing notebooks to heroku

--- frontend/reducers/notes_reducer.js
+++ frontend/reducers/notes_reducer.js
@@ -11,7 +11,6 @@ import { merge } from 'lodash';
 const initialState = {};
 
 export default (state = initialState, action) => {
-  debugger
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_NOTES:
